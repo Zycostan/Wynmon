989e7f9122898f9efff9abc8bf84fbc0b1cc15b2
Blobified <87593231+Zycostan@users.noreply.github.com>
2026-08-26 08:39:19 +0800

    >_<

--- scripts/content/targeted_effects/add_condition.gd
+++ scripts/content/targeted_effects/add_condition.gd
@@ -8,7 +8,7 @@ extends TargetedEffect
 func _do(doer: Monster, source: Object, game_state: GameState, is_critical: bool, logs: Array[String]):
 	var target = doer if target_self else MonsterController.get_monster_opponent(doer)
 	
-	if GameRunner.rng.randf() > chance_to_apply:
+	if GameRunner.rng.randf() < chance_to_apply:
 		
 		MonsterController.instantiate_condition_on_monster(target, condition_resource)
 	
